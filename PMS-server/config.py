b6deea7c6089b637f23ba4850ed6b1152a3696e9
Lime <paganinist@gmail.com>
2016-08-14 21:58:45 +0900

    Update config.py
    Set Database IP address to docker0 bridge address

--- PMS-server/config.py
+++ PMS-server/config.py
@@ -5,7 +5,7 @@ DEBUG = True
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = "mysql://root:root@localhost/patients?charset=utf8"
+SQLALCHEMY_DATABASE_URI = "mysql://root:root@172.17.0.1/patients?charset=utf8"
 
 THREADS_PER_PAGE = 2
 CSRF_ENABLED = True
